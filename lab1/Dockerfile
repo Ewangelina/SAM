cf21a20dfe5d60aea0f79933d141493dfb60c4cd
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-03-06 14:59:52 +0100

    Dodanie plików lab0

---
+++ lab1/Dockerfile
@@ -0,0 +1,10 @@
+FROM openjdk
+
+WORKDIR /usr/src/app
+
+
+COPY ./source/ ./
+EXPOSE 4080
+
+CMD [ "/bin/bash", "start.sh" ]
+
---
+++ lab1/source/server.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(4080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello World!\n";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+	    System.out.println("Served hello world...");
+        }
+    }
+
+}
