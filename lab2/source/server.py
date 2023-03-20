398452be3c28c3908375409436ceb05eccc60ca7
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-03-20 15:07:38 +0100

    script 2

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -12,7 +12,8 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 	def do_GET(self):
 
 		print(self.path)
-       
+		audio_flag = 0
+		video_flag = 0
         
 		self.protocol_version = 'HTTP/1.1'
 		self.send_response(200)
@@ -22,29 +23,53 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 		params = self.path.split("?")
 		try:
 			parsed_url = parse_qs(params[1])
+			
+			try:
+				parsed_url['imgFile'][0]
+				self.wfile.write(b"<img id=\"posterImage\" src=\"")
+				self.wfile.write(bytes(parsed_url['imgFile'][0], encoding="utf-8"))
+				self.wfile.write(b"\">")
+						 
+			except:
+				self.wfile.write(b"NO IMAGE\n")
 				
 			try:
 				parsed_url['videoFile'][0]
-				self.wfile.write(b"<video width=\"320\" height=\"240\" controls>")
+				self.wfile.write(b"<video id=\"videoPlayer\" width=\"320\" height=\"240\" controls>")
 				self.wfile.write(b"<source src=\"")
 				self.wfile.write(bytes(parsed_url['videoFile'][0], encoding="utf-8"))
 				self.wfile.write(b"\" type=\"video\">")
 				self.wfile.write(b"</video>")
+				video_flag = 1
 				
 			except:
 				self.wfile.write(b"NO VIDEO\n")
 				
 			try:
 				parsed_url['audioFile'][0]
-				self.wfile.write(b"<audio controls>")
+				self.wfile.write(b"<audio id=\"audioPlayer\" controls>")
 				self.wfile.write(b"<source src=\"")
 				self.wfile.write(bytes(parsed_url['audioFile'][0], encoding="utf-8"))
 				self.wfile.write(b"\" type=\"audio\">")
 				self.wfile.write(b"</audio>")
+				audio_flag = 1
 			except:
 				self.wfile.write(b"NO AUDIO\n")
 		except:
 			self.wfile.write(b"NO ARGUMENTS\n")
+			
+			
+		if video_flag == 1:
+			self.wfile.write(b"<button id = \"videoCancel\" onclick = \"cancelVideo()\"> Video cancel </button>")
+			self.wfile.write(b"<script>")
+			self.wfile.write(b"function cancelVideo() {")
+			self.wfile.write(b"var para1 = document.getElementById(\"videoPlayer\");")
+			
+			self.wfile.write(b"para1.innerHTML = \"<source src=\"cancel.mp4\">\";")
+			self.wfile.write(b"}<\script>")
+			
+		if audio_flag == 1:
+			self.wfile.write(b"<button id = \"audioCancel\" onclick = \"cancelAudio()\"> Audio cancel </button>")
 		        
     
 # --- main ---
