5ddcecab9f466e14803414b3d7848a32e7cbd6bd
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-05-15 14:55:55 +0200

    added buttons

--- lab5/source/server.js
+++ lab5/source/server.js
@@ -8,6 +8,30 @@ app.get("/", function(req, res) {
     const posterImage = req.query.imgFile;
     
     let ret = "";
+    ret += (`<script>`);
+    ret += `	function moveUp(row_num)`;
+ret += `	{`;
+ret += `		var first = row_num;`;
+ret += `		if (row_num == 0)`;
+ret += `		{`;
+ret += `			first = document.getElementById('playlist_table').getElementsByTagName('tbody')[0].rows.length -1;`;
+ret += `		}`;
+ret += `		var rows = document.getElementById('playlist_table').rows,`;
+ret += `		parent.insertAfter(rows[first],rows[row_num - 1]);`;
+ret += `	}`;
+ret += (`</script>`);
+ret += (`<script>`);
+ret += `	function moveDown(row_num)`;
+ret += `	{`;
+ret += `		var first = row_num - 2;`;
+ret += `		if (document.getElementById('playlist_table').getElementsByTagName('tbody')[0].rows.length == rownum)`;
+ret += `		{`;
+ret += `			first = 0;`;
+ret += `		}`;
+ret += `		var rows = document.getElementById('playlist_table').rows,`;
+ret += `		parent.insertBefore(rows[first],rows[row_num - 1]);`;
+ret += `	}`;
+ret += (`</script>`);
   
     res.set(`Content-Type`, `text/html`);
 
@@ -20,16 +44,17 @@ app.get("/", function(req, res) {
         ret += (`<script>function cancel_video(){ document.getElementById("videoPlayer").src = "cancel.mp4";}</script>`);
                 
 	ret += (`<script>`);
-	ret += (`	function add_video()`);
-	ret += (`	{`);
-	ret += (`		let tableRef = document.getElementById('playlist_table').getElementsByTagName('tbody')[0]; `);
-	ret += (`		let myHtmlContent = "<tr><td>" + tableRef.rows.length + "</td><td>";`);
-	ret += (`		myHtmlContent += document.getElementById('videoPlayer').src;`);
-	ret += (`		myHtmlContent += "</td><td>Video</td>";`);
-	ret += (`		myHtmlContent += "<td> <button type='button' class = 'removeRowButton' onclick = this.parentElement.parentElement.remove()>Delete</button> </td></tr>";`);
-	ret += (`		let newRow = tableRef.insertRow(tableRef.rows.length);`);
-	ret += (`		newRow.innerHTML = myHtmlContent;`);
-	ret += (`	}`);
+	ret += `	function add_video()	{`;
+ret += `	let tableRef = document.getElementById('playlist_table').getElementsByTagName('tbody')[0];`;
+ret += `	let myHtmlContent = "<tr><td>" + tableRef.rows.length + "</td><td>";`;
+ret += `	myHtmlContent += document.getElementById('videoPlayer').src;`;
+ret += `	myHtmlContent += "</td><td>Video</td>";`;
+ret += `	myHtmlContent += "<td> <button type='button' class = 'removeRowButton' onclick = this.parentElement.parentElement.remove()>Delete</button>";`;
+ret += `	myHtmlContent += "<button type='button' class = 'moveRowUpButton' onclick = moveUp(this.parentElement.parentElement.childElement.innerHTML)>Up</button>";`;
+ret += `	myHtmlContent += "<button type='button' class = 'moveRowDownButton' onclick = moveDown(this.parentElement.parentElement.childElement.innerHTML)>Dow</button></td></tr>";`;
+ret += `	let newRow = tableRef.insertRow(tableRef.rows.length);`;
+ret += `	newRow.innerHTML = myHtmlContent;	`;
+ret += `	}`;
 	ret += (`</script>`);
 	ret += (`<button id = "videoCancel" onclick = "cancel_video()">Cancel video</button>`);
 	ret += (`<button id = "videoAdd" onclick = "add_video()">Add video</button><br>`);
