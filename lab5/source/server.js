b1956965590603f2394bc63debf818e227c6ec8e
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-05-15 15:19:30 +0200

    Change

--- lab5/source/server.js
+++ lab5/source/server.js
@@ -2,126 +2,149 @@ const express = require('express')
 
 const app = express()
 
-app.get("/", function(req, res) {
-    const videoFile = req.query.videoFile;
-    const audioFile = req.query.audioFile;
-    const posterImage = req.query.imgFile;
-    
-    let ret = "";
-    ret += (`<script>`);
-    ret += `	function moveUp(row_num)`;
-ret += `	{`;
-ret += `		var first = row_num;`;
-ret += `		if (row_num == 0)`;
-ret += `		{`;
-ret += `			first = document.getElementById('playlist_table').getElementsByTagName('tbody')[0].rows.length -1;`;
-ret += `		}`;
-ret += `		var rows = document.getElementById('playlist_table').rows,`;
-ret += `		parent.insertAfter(rows[first],rows[row_num - 1]);`;
-ret += `	}`;
-ret += (`</script>`);
-ret += (`<script>`);
-ret += `	function moveDown(row_num)`;
-ret += `	{`;
-ret += `		var first = row_num - 2;`;
-ret += `		if (document.getElementById('playlist_table').getElementsByTagName('tbody')[0].rows.length == rownum)`;
-ret += `		{`;
-ret += `			first = 0;`;
-ret += `		}`;
-ret += `		var rows = document.getElementById('playlist_table').rows,`;
-ret += `		parent.insertBefore(rows[first],rows[row_num - 1]);`;
-ret += `	}`;
-ret += (`</script>`);
-  
-    res.set(`Content-Type`, `text/html`);
+app.get('/', (req, res) => {
+	let ret = '';
+	if (req.query.audioFile) {
+		ret += `<audio id = "audioPlayer" src = ${req.query.audioFile}></audio>`;
+		ret += `<button type="button" id = "audioCancel">cancel audio</button>`;
+		ret += `<button type="button" id = "audioAdd">Add audio</button>`;
+		ret += `<script>document.getElementById("audioCancel").addEventListener('click', 
+		() => document.getElementById("audioPlayer").src = "cancel.mp3")</script>`
 
-        
-    if(videoFile)
-    {
-        ret += (`<video id = "videoPlayer" src="` + videoFile + `" controls></video>`);
-        ret += (`<BR>`);
-        
-        ret += (`<script>function cancel_video(){ document.getElementById("videoPlayer").src = "cancel.mp4";}</script>`);
-                
-	ret += (`<script>`);
-	ret += `	function add_video()	{`;
-ret += `	let tableRef = document.getElementById('playlist_table').getElementsByTagName('tbody')[0];`;
-ret += `	let myHtmlContent = "<tr><td>" + tableRef.rows.length + "</td><td>";`;
-ret += `	myHtmlContent += document.getElementById('videoPlayer').src;`;
-ret += `	myHtmlContent += "</td><td>Video</td>";`;
-ret += `	myHtmlContent += "<td> <button type='button' class = 'removeRowButton' onclick = this.parentElement.parentElement.remove()>Delete</button>";`;
-ret += `	myHtmlContent += "<button type='button' class = 'moveRowUpButton' onclick = moveUp(this.parentElement.parentElement.childElement.innerHTML)>Up</button>";`;
-ret += `	myHtmlContent += "<button type='button' class = 'moveRowDownButton' onclick = moveDown(this.parentElement.parentElement.childElement.innerHTML)>Dow</button></td></tr>";`;
-ret += `	let newRow = tableRef.insertRow(tableRef.rows.length);`;
-ret += `	newRow.innerHTML = myHtmlContent;	`;
-ret += `	}`;
-	ret += (`</script>`);
-	ret += (`<button id = "videoCancel" onclick = "cancel_video()">Cancel video</button>`);
-	ret += (`<button id = "videoAdd" onclick = "add_video()">Add video</button><br>`);
-        
-        
-    }
-    else
-    {
-        ret += (`<p>No video file</p><BR>`);
-    }
+	}
+	if (req.query.videoFile) {
+		ret += `</br> <video id = "videoPlayer" src = ${req.query.videoFile}></video>`;
+		ret += `<button type="button" id = "videoCancel">cancel video</button>`;
+		ret += `<button type="button" id = "videoAdd">Add video</button>`;
+		ret += `<script>document.getElementById("videoCancel").addEventListener('click', 
+		() => document.getElementById("videoPlayer").src = "cancel.mp4")</script>`
+	}
+	if (req.query.imgFile) {
+		ret += `<img src=${req.query.imgFile} id = "posterImage">`;
+		ret += `<button type="button" id = "imgCancel">cancel image</button>`;
+		ret += `<button type="button" id = "imgAdd">Add img</button>`;
+		ret += `<script>document.getElementById("imgCancel").addEventListener('click', 
+		() => document.getElementById("posterImage").src = "cancel.jpg")</script>`
+	}
+	ret += `<table id = 'playlist_table'> 
+					  <tr>
+    					<th>No.</th>
+    					<th>URL</th>
+    				  	<th>Type</th>
+    				  	<th>Action</th>th>
+  					  </tr>
+					</table>`
 
-    if(audioFile){
-        ret += (`<audio id="audioPlayer" src="` + audioFile +`" controls/>`);
-        ret += (`</audio><BR>`);
+	ret += `
+	<script>
+	let addDeleteToDeleteButtons = () => {
+        		const deleteButtons = document.querySelectorAll('.removeRowButton')
+		deleteButtons.forEach( (button) => {
+            
+            button.addEventListener('click', () => {
+                const row = button.parentElement.parentElement;
+                row.remove();
+            })
+		})
+	}
+	</script>
+	`
 
-	ret += (`<script>function cancel_audio(){ document.getElementById("audioPlayer").src = "cancel.mp3";}</script>`);
-        ret += (`<button id="audioCancel" onclick="cancel_audio()">Cancel audio</button>`);
-        
-        ret += (`<script>`);
-	ret += (`	function add_audio()`);
-	ret += (`	{`);
-	ret += (`		let myHtmlContent = "<tr><td>" + document.getElementById("playlist_table").tBodies[0].rows.length.toString() + "</td><td>";`);
-	ret += (`		myHtmlContent += document.getElementById('audioPlayer').src;`);
-	ret += (`		myHtmlContent += "</td><td>Audio</td>";`);
-	ret += (`		myHtmlContent += "<td> <button type='button' class = 'removeRowButton' onclick = this.parentElement.parentElement.remove()>Delete</button> </td></tr>";`);
-	ret += (`		var tableRef = document.getElementById('playlist_table').getElementsByTagName('tbody')[0];`);
-	ret += (`		var newRow = tableRef.insertRow(tableRef.rows.length);`);
-	ret += (`		newRow.innerHTML = myHtmlContent;`);
-	ret += (`	}`);
-	ret += (`</script>`);
+	ret += `
+	<script>
+	let addMoveRowUpToUpButton = (button) => {
+            const row = button.parentElement.parentElement;
+			let prevRow = row.previousElementSibling;
+			if (prevRow.rowIndex > 0) {
+				row.parentNode.insertBefore(row, prevRow)
+			}
+			else {
+				row.parentNode.appendChild(row);
+			}
+	}
+	</script>
+	`
+
+	ret += `
+	<script>
+		let insertAfter = (newNode, referenceNode) => {
+			referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+		}
+	</script>`
+
 	
-	ret += (`<script>`);
-	ret += (`	function delete_row(num)`);
-	ret += (`	{`);
-	ret += (`		document.getElementById('playlist_table').deleteRow(num); `);
-	ret += (`	}`);
-	ret += (`</script>`);
-	    
-        ret += (`<button id = "audioAdd" onclick = "add_audio()">Add audio</button><br>`);
-    }
-    else
-    {
-        ret += (`<p>No audio file</p><BR>`);
-    }
+	ret += `
+	<script>
+	let addMoveRowDownToDownButton = (button) => {
+            
+                const row = button.parentElement.parentElement;
+				let prevRow = row.nextElementSibling;
+				let firstRow = document.querySelector('tr');
+				if (row.rowIndex < document.querySelectorAll('tr').length - 1) {
+					insertAfter(row, prevRow);
+				}
+				else if (firstRow) {
+					insertAfter(row, firstRow);
+				}
+	}
+	</script>
+	`
+
+	if (req.query.audioFile) {
+		ret += `<script>document.getElementById("audioAdd").addEventListener('click',
+		() => {
+    			let table = document.getElementById('playlist_table');
+                let row = table.insertRow(table.rows.length);
+    			let cell1 = row.insertCell(0);
+				let cell2 = row.insertCell(1);
+            	let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+            	
+            	cell1.innerHTML = table.rows.length - 1;
+            	cell2.innerHTML = document.getElementById('audioPlayer').src;
+            	cell3.innerText = 'Audio';
+                cell4.innerHTML = '<button type="button" class="removeRowButton">Delete</button> <button type="button" class="moveRowUpButton" onclick="addMoveRowUpToUpButton(this)">Up</button> <button type="button" class="moveRowDownButton" onclick="addMoveRowDownToDownButton(this)">Down</button>';
+                addDeleteToDeleteButtons();
+		})</script>`
+	}
+
+	if (req.query.videoFile) {
+		ret += `<script>document.getElementById('videoAdd').addEventListener('click',
+		() => {
+    		let table = document.getElementById('playlist_table');
+            let row = table.insertRow(table.rows.length);
+			let cell1 = row.insertCell(0);
+			let cell2 = row.insertCell(1);
+			let cell3 = row.insertCell(2);
+            let cell4 = row.insertCell(3);
+			cell1.innerHTML = table.rows.length - 1;
+			cell2.innerHTML = document.getElementById('videoPlayer').src
+			cell3.innerText = 'Video';
+            cell4.innerHTML = '<button type="button" class="removeRowButton">Delete</button> <button type="button" class="moveRowUpButton" onclick="addMoveRowUpToUpButton(this)">Up</button> <button type="button" class="moveRowDownButton" onclick="addMoveRowDownToDownButton(this)">Down</button>';
+            addDeleteToDeleteButtons();
+		})</script>`
+	}
+
+	if (req.query.imgFile) {
+		ret += `<script>document.getElementById("imgAdd").addEventListener('click',
+		() => {
+    			let table = document.getElementById('playlist_table');
+                let row = table.insertRow(table.rows.length);
+    			let cell1 = row.insertCell(0);
+				let cell2 = row.insertCell(1);
+            	let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+            	cell1.innerHTML = table.rows.length - 1;
+            	cell2.innerHTML = document.getElementById('posterImage').src;
+            	cell3.innerText = 'Image';
+                cell4.innerHTML = '<button type="button" class="removeRowButton">Delete</button> <button type="button" class="moveRowUpButton" onclick="addMoveRowUpToUpButton(this)">Up</button> <button type="button" class="moveRowDownButton" onclick="addMoveRowDownToDownButton(this)">Down</button>';
+                addDeleteToDeleteButtons();
+		})</script>`
+	}
 
 
-    if(posterImage)
-    {
-        ret += (`<img src="` + posterImage +`" id = "posterImage">`);
-    }
-    else
-    {
-        ret += (`<p>No poster image</p><BR>`);
-    }
-	
-    //table
-    ret += (`<table id = playlist_table><tr>`);
-    ret += (`<th>No</th>`);
-    ret += (`<th>URL</th>`);
-    ret += (`<th>Type</th>`);
-    ret += (`<th>Action</th>`);
-    ret += (`</tr>`);
-    ret += (`</table>`);    
-    
+
     res.send(ret);
-  });
-
-
+})
 
 app.listen(4080)
